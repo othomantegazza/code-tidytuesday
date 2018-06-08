5db0279fd1c99260b1b66fcc62ff9eca1fba7cd2
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-06-08 14:46:56 +0200

    draft plot with map of Portland

--- 010-biketown.R
+++ 010-biketown.R
@@ -1,2 +1,15 @@
+library(ggmap)
 library(tidyverse)
 
+dat <- read_csv("data/week10_biketown.zip")
+
+# Map inspired by https://twitter.com/WireMonkey/status/1004790383451176962
+portland <- get_map("Portland, Oregon",
+                    zoom = 13,
+                    # source = "stamen",
+                    maptype = "toner-lite")
+ggmap(portland) +
+  geom_point(data = dat %>% 
+               select(StartLatitude, StartLongitude),
+             aes(x = StartLongitude,
+                 y = StartLatitude))
