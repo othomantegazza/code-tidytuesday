aaa352d5ccfdf979bb2e950edec70ea709b7d7d5
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-06-09 15:55:35 +0200

    Use geom_point() instead of geom_count()
    Because geom_count() does not accept aes frame
    for gganimate

--- 010-biketown.R
+++ 010-biketown.R
@@ -12,7 +12,7 @@ dat <- read_csv("data/week10_biketown.csv") %>%
   # A column with weekdays will be useful later
   mutate(start_day = weekdays(StartDate)) %>%
   mutate(start_hour = hour(StartTime)) %>%
-  mutate(start_month = month(StartDate))
+  mutate(start_month = as.factor(month(StartDate)))
   
 
 # Get Map -----------------------------------------------------------------
@@ -62,12 +62,24 @@ dev.off()
 
 
 # Make gif with months ----------------------------------------------------
+dat <- dat %>%
+  group_by(start_month, StartLatitude, StartLongitude) %>%
+  summarise(n = n())
 
+
+# inspired by https://d4tagirl.com/2017/05/how-to-plot-animated-maps-with-gganimate
 library(gganimate)
 p <- ggmap(portland) +
-  geom_count(data = dat,
+  geom_point(data = dat,
              aes(x = StartLongitude,
                  y = StartLatitude,
-                 frame = start_month))
+                 size = n,
+                 frame = start_month),
+             alpha = .3) 
+  
+
+# p$mapping <- aes(x = lon,
+#                  y = lat,
+#                  frame = start_month)
 
 gganimate(p)
