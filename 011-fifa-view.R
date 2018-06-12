e123196ec1b877ad631d7318c2a3bd496ff3c905
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-06-12 18:18:54 +0200

    Plot share in europe

--- 011-fifa-view.R
+++ 011-fifa-view.R
@@ -1,5 +1,7 @@
 library(tidyverse)
-
+library(maptools)
+library(maps)
+library(broom)
 
 # Get data ----------------------------------------------------------------
 
@@ -17,22 +19,50 @@ if(!file.exists(dat_path)) {
 }
 
 
-# Share per country? -----------------------------------------
+# Share density in europe? -----------------------------------------
 
-dat <- dat %>% 
-  arrange(desc(tv_audience_share)) %>%
-  mutate(rank = 1:nrow(.)) %>%
-  filter(rank <= 50)
+dat <- dat %>%
+  filter(confederation == "UEFA") %>%
+  arrange(desc(tv_audience_share))
 
 
-# Plot rank share by country ----------------------------------------------
+# Europe Map --------------------------------------------------------------
 
-ggplot(data = dat,
-       aes(x = rank,
-           y = tv_audience_share)) + 
-  # geom_point(alpha = .01) + 
-  geom_linerange(aes(ymin = 0, ymax = tv_audience_share), lwd = 4) + 
-  geom_hline(yintercept = 0, lwd = .05) +
-  geom_rug(aes(x = rank, y = NULL), alpha = .5) +
-  facet_wrap(facets = "confederation", ncol = 1) +
-  theme_bw()
+data("wrld_simpl")
+dat$country[!dat$country %in% wrld_simpl$NAME]
+
+country_codes <- wrld_simpl@data %>%
+  rename(country = "NAME",
+         id = "ISO3") %>%
+  select(country, id)
+
+dat <- dat %>%
+  left_join(country_codes)
+
+uefa <- wrld_simpl[wrld_simpl$NAME %in% dat$country, ]
+
+# plot(uefa)
+# https://www.r-graph-gallery.com/a-smooth-transition-between-chloropleth-and-cartogram/
+
+uefa <- tidy(uefa) %>%
+  left_join(dat) %>%
+  group_by(id) %>%
+  mutate(med_long = mean(range(long)),
+         med_lat = mean(range(lat))) %>%
+  ungroup()
+
+ggplot(data = uefa,
+       aes(fill = tv_audience_share,
+           x = long,
+           y = lat,
+           group = group,
+           label = tv_audience_share)) +
+  geom_polygon(size=0, alpha=0.9) +
+  geom_text(aes(x = med_long,
+                y = med_lat,
+                size = tv_audience_share*2),
+            colour = "darkgrey") +
+  xlim(-10, 50) +
+  ylim(32, 77) +
+  theme_void()
+
