233cd18b85bfaeefbdc5b50ffc79653a7f26a1b9
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-06-20 14:35:11 +0200

    Tidy data, group by hurricane

--- 012-hurricane-cover.R
+++ 012-hurricane-cover.R
@@ -8,11 +8,11 @@ local_data <- "data/hurrican-cover.Rdata"
 
 if(!file.exists(local_data)) {
   paths <- c(google_trends = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_google_trends.csv",
-             hurricanes = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_mediacloud_hurricanes.csv",
+             written_media = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_mediacloud_hurricanes.csv",
              states = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_mediacloud_states.csv",
              top_online_news = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_mediacloud_top_online_news.csv",
              trump = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_mediacloud_trump.csv",
-             tv_hurricane = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_tv_hurricanes.csv")
+             tv_media = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/week12_tv_hurricanes.csv")
   
   dat <- paths %>%
     map(read_csv)
@@ -35,25 +35,60 @@ if(!file.exists(local_data)) {
 }
 
 
-# Small tidying -----------------------------------------------------------
+# Small tidyings  to specific files------------------------------------------------
 
-dat$hurricanes <- dat$hurricanes %>% mutate(Date = mdy(Date))
+dat$written_media <- dat$written_media %>% mutate(Date = mdy(Date))
 dat$states <- dat$states %>% mutate(Date = mdy(Date))
-dat$tv_hurricane <- dat$tv_hurricane %>% 
+dat$tv_media <- dat$tv_media %>% 
   mutate(Date = mdy(Date)) %>%
   rename_at(.vars = vars(Harvey:Jose), .funs = ~paste0(., "_tv"))
 dat$top_online_news <- NULL
 dat$google_trends <- dat$google_trends %>% 
   rename(Date = "Day") 
 
-tst <- dat %>% reduce(full_join)
 
 
-# Hurricane measures ------------------------------------------------------
+# Tidy and merge into one dataset -----------------------------------------
 
-hurs <- dat[c(1,2,5)] %>%
-  reduce(full_join)
+dat <- dat %>%
+  map(~gather(data = .,
+              key = "hurricane_state",
+              value = "value",
+              -Date))
 
-hurs %>%
-  select(contains("Irma")) %>%
-  pairs
+dat <- names(dat) %>%
+  map(~mutate(dat[[.]], source = .)) %>%
+  reduce(bind_rows)
+
+
+# Plot reaction -----------------------------------------------------------
+
+plot_hur <- function(hur = "Harvey") 
+{
+  hur_dat <- dat %>%
+    filter(grepl(hur, hurricane_state))
+  
+  p <- ggplot(hur_dat,
+              aes(x = Date, y = value)) +
+    geom_col() +
+    facet_grid(source ~ ., scales = "free_y") +
+    theme_bw()
+  
+  return(p)
+}
+
+plot_hur()
+plot_hur(hur = "Irma")
+plot_hur(hur = "Maria")
+plot_hur(hur = "Jose")
+# Split by hurricane ------------------------------------------------------
+
+split_by_hur <- function(hur_id = "Irma",
+                         hur_state = "Texas") 
+{
+  hur_id <- enquo(hur_id)
+  dat %>% 
+    select(Date, contains(!!hur_id)) 
+}
+
+split_by_hur()
