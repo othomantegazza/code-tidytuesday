c28d601b9a035a3978fe5ad5efe9f120d2aea8ce
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-06-20 17:34:50 +0200

    plot with facetting

--- 012-hurricane-cover.R
+++ 012-hurricane-cover.R
@@ -1,3 +1,4 @@
+Sys.setlocale("LC_TIME", "en_US.UTF8")
 library(tidyverse)
 library(lubridate)
 
@@ -63,32 +64,71 @@ dat <- names(dat) %>%
 
 # Plot reaction -----------------------------------------------------------
 
-plot_hur <- function(hur = "Harvey") 
-{
-  hur_dat <- dat %>%
-    filter(grepl(hur, hurricane_state))
-  
-  p <- ggplot(hur_dat,
-              aes(x = Date, y = value)) +
-    geom_col() +
-    facet_grid(source ~ ., scales = "free_y") +
-    theme_bw()
-  
-  return(p)
-}
+dat <- dat %>%
+  # Simple names for hurricanes
+  mutate(hurricane = case_when(grepl("Harvey", hurricane_state) ~"Harvey",
+                               grepl("Irma", hurricane_state) ~ "Irma",
+                               grepl("Jose", hurricane_state) ~ "Jose",
+                               grepl("Maria", hurricane_state) ~ "Maria",
+                               TRUE ~ NA_character_)) %>%
+  # descriptive name for data sources
+  mutate(source = case_when(source == "google_trends" ~ "Google Search Trend",
+                            source == "tv_media" ~ "TV Share",
+                            source == "written_media" ~ "N. of Sentences in Online News")) %>%
+  filter(complete.cases(.),
+         value > 0)
+
+jpeg(filename = "plots/hurricane_media.jpg",
+     width = 8, 
+     height = 5,
+     units = "in",
+     res = 200)
+ggplot(dat,
+       aes(x = Date, y = value)) +
+  geom_col(fill = "#007399",
+           alpha = .5) + #0099cc") +
+  facet_grid(source ~ hurricane,
+             scales = "free",
+             space = "free_x",
+             labeller = label_wrap_gen(width = 10,
+                                       multi_line = T)) +
+  ggtitle("Hurricane Mentions in Media") +
+  ylab("Recurrence in Media") +
+  theme_bw() +
+  theme(text = element_text(size = 12),
+        axis.text.x = element_text(angle = 90, vjust = 1),
+        strip.text.y = element_text(angle = 0))
+dev.off()
+
+# plot_hur <- function(hur = "Harvey") 
+# {
+#   hur_dat <- dat %>%
+#     filter(grepl(hur, hurricane_state)) %>%
+#     filter(value > 0)
+#   
+#   p <- ggplot(hur_dat,
+#               aes(x = Date, y = value)) +
+#     geom_col() +
+#     facet_grid(source ~ ., scales = "free_y") +
+#     theme_bw()
+#   
+#   return(p)
+# }
 
 plot_hur()
 plot_hur(hur = "Irma")
 plot_hur(hur = "Maria")
 plot_hur(hur = "Jose")
+
 # Split by hurricane ------------------------------------------------------
 
-split_by_hur <- function(hur_id = "Irma",
-                         hur_state = "Texas") 
-{
-  hur_id <- enquo(hur_id)
-  dat %>% 
-    select(Date, contains(!!hur_id)) 
-}
 
-split_by_hur()
+# split_by_hur <- function(hur_id = "Irma",
+#                          hur_state = "Texas") 
+# {
+#   hur_id <- enquo(hur_id)
+#   dat %>% 
+#     select(Date, contains(!!hur_id)) 
+# }
+# 
+# split_by_hur()
