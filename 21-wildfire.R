1db1d6af394b04247fd121847e019c397a417513
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-08-24 16:26:54 +0200

    remove exploratory lines from wildfires

--- 21-wildfire.R
+++ 21-wildfire.R
@@ -1,25 +1,9 @@
 library(tidyverse)
 library(lubridate)
-library(viridis)
-library(ggridges)
-
-dat <- read_csv("~/Desktop/2018-07-wildfire-trends-master/data/calfire_frap.csv")
-
-
-fire_year <- dat  %>%
-  mutate(alarm_year = year(alarm_date)) %>%
-  group_by(alarm_year, state) %>%
-  summarise(gis_acres = sum(gis_acres, na.rm = TRUE))
-
-# load data
-files <- list.files("data/us_fires")
-
-us_fires <- data_frame()
-
 
 # load USA wide data ------------------------------------------------------
 
-paths <- list.files("~/Desktop/2018-07-wildfire-trends-master/data/us_fires",
+paths <- list.files("data/us_fires",
                     full.names = T)
 
 us_fires <- paths %>%
@@ -41,18 +25,16 @@ us_fires <- paths %>%
 
 # Bin data ----------------------------------------------------------------
 
-tst <- us_fires %>%
-  mutate(latitude = round(latitude, digits = 0),
-         longitude = round(longitude, digits = 0)) %>%
-  group_by(fire_year, latitude, longitude) %>%
-  summarise(fire_size = sum(fire_size))
-
 jpeg(filename = "plots/21-wildfire-map.jpg",
      width = 10, 
      height = 7,
      units = "in",
      res = 200)
-tst %>%
+us_fires %>%
+  mutate(latitude = round(latitude, digits = 0),
+         longitude = round(longitude, digits = 0)) %>%
+  group_by(fire_year, latitude, longitude) %>%
+  summarise(fire_size = sum(fire_size)) %>%
   # filter(fire_year == 2007) %>%
   ggplot(aes(x = longitude,
              y = latitude,
@@ -62,78 +44,12 @@ tst %>%
   # coord_map(projection = "azequalarea") +
   ylim(20, 50) +
   xlim(-130, -70) +
-  scale_fill_viridis(option = "C") +
+  scale_fill_viridis_c(option = "C") +
   theme_bw() 
 dev.off()
 
-# plot map ----------------------------------------------------------------
 
-jpeg(filename = "plots/21-wildfire.jpg",
-     width = 9, 
-     height = 9,
-     units = "in",
-     res = 200)
-us_fires %>% 
-  filter(fire_year == 2007) %>%
-  ggplot(aes(x = longitude,
-             y = latitude)) +
-  geom_point() +
-  coord_map(projection = "azequalarea") +
-  ylim(20, 50) +
-  xlim(-130, -70) +
-  theme_bw()
-dev.off()
- 
-
-# Total intensity by country? ---------------------------------------------
-
-us_fires %>%
-  group_by(county,
-           fire_year) %>%
-  summarise(fire_size = sum(fire_size)) %>%
-  ggplot(aes(x = fire_year,
-             y = fire_size)) +
-  # geom_point(size = 3) +
-  geom_boxplot() +
-  # scale_y_log10() +
-  coord_flip() +
-  theme_bw()
-
-us_fires %>%
-  group_by(county,
-           fire_year) %>%
-  summarise(fire_size = sum(fire_size)) %>%
-  ggplot(aes(x = fire_size,
-             y = fire_year)) +
-  geom_density_ridges(alpha = .3) +
-  scale_x_log10() +
-  theme_ridges()
-
-us_fires %>%
-  group_by(state,
-           fire_year) %>%
-  summarise(fire_size = sum(fire_size)) %>%
-  ggplot(aes(x = fire_year,
-             y = state)) +
-  # geom_point(aes(size = fire_size), alpha = .3) +
-  geom_raster(aes(fill = sqrt(fire_size))) +
-  scale_fill_viridis(option = "C") +
-  # geom_boxplot() +
-  # scale_y_log10() +
-  # coord_flip() +
-  theme_bw()
-
-us_fires %>%
-  ggplot(aes(x = fire_size)) +
-  geom_histogram() +
-  facet_grid(. ~ fire_size_class, scales = "free")
-
-us_fires %>% 
-  # group_by(fire_year, fire_size_class) %>%
-  # tally()
-  ggplot(aes(x = fire_year)) +
-  geom_histogram(stat = "count") +
-  facet_grid(fire_size_class ~ ., scale = "free_y")
+# is total intensity increasing ---------------------------------------------
 
 jpeg(filename = "plots/21-wildfire-high-class.jpg",
      width = 10, 
@@ -147,9 +63,9 @@ us_fires %>%
   # geom_point(alpha = .1) +
   geom_hex(bins = 70) +
   scale_y_log10() +
-  scale_fill_viridis(option = "C") +
+  scale_fill_viridis_c(option = "C") +
   theme_minimal(base_size = 20) +
-  ggtitle("Big fires in the USA from 1992 to 2015",
+  ggtitle("Big Fires in the USA from 1992 to 2015",
           sub = "Data from US fire service, only biggest fires (categories F and G)") +
   xlab("Date of Alert") +
   ylab("Fire Size [Hectares???] (log scale)")
@@ -167,22 +83,18 @@ us_fires %>%
   # geom_point(alpha = .1) +
   geom_hex(bins = 12) +
   scale_y_log10() +
-  scale_fill_viridis(option = "C") +
+  scale_fill_viridis_c(option = "C") +
   scale_x_datetime(date_breaks = "3 months",
                    date_labels = "%b") +
   facet_wrap(facets = "fire_year",
              scales = "free_x",
              ncol = 6) +
   theme_bw(base_size = 14) +
-  ggtitle("Big fires in the USA from 1992 to 2015",
+  ggtitle("Big Fires in the USA from 1992 to 2015",
         sub = "Data from US fire service, only biggest fires (categories F and G)") +
   xlab("Date of Alert") +
   ylab("Fire Size [Hectares???] (log scale)")
 dev.off()
 
-
-us_fires %>%
-  split(.$fire_size_class) %>%
-  map(~range(.$fire_size))
 
   
