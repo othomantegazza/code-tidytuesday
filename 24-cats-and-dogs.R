cb516d22ff0a88880fee5c661a3a043f89250c8e
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-09-11 23:04:41 +0200

    Try Dotplot visualization as in socviz

--- 24-cats-and-dogs.R
+++ 24-cats-and-dogs.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-
+library(statebins)
+library(socviz)
 
 # Get data ----------------------------------------------------------------
 
@@ -15,4 +16,46 @@ if(!file.exists(dat_path)) {
 } else {
   load(dat_path)
 }
+
+
+# plot --------------------------------------------------------------------
+
+# replicate https://socviz.co/maps.html#map-u.s.-state-level-data
+
+# get the census column from socviz
+# to facet macro-regions
+
+tst <- dat %>%
+  left_join(socviz::election %>%
+              select(state, census)) %>%
+  mutate(ratio_owner = percent_dog_owners/percent_cat_owners,
+         main_household = case_when(ratio_owner > 1 ~ "Dog",
+                                    ratio_owner < 1 ~ "Cat",
+                                    TRUE ~ "Both"))
+
+# tst <- dat %>%
+#   left_join(socviz::election %>%
+#               select(state, census)) %>%
+#   mutate(ratio_owner = avg_dogs_per_household/avg_cats_per_household,
+#          main_household = case_when(ratio_owner > 1 ~ "Dog",
+#                                     ratio_owner < 1 ~ "Cat",
+#                                     TRUE ~ "Both"))
+#   
+
+tst %>%
+  ggplot(aes(x = reorder(state, ratio_owner),
+             y = ratio_owner,
+             colour = main_household)) +
+  geom_point(size = 2) +
+  geom_hline(yintercept = 1) +
+  facet_grid(census ~ .,
+             scales = "free_y",
+             space = "free") +
+  coord_flip() +
+  theme_minimal()
+
+
+
+
+
 
