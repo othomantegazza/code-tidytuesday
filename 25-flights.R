77a4e216059051ce006cf29f0b0a677e6cb37b11
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-09-22 23:55:33 +0200

    use rlm, predict and plot ribbon
    also label outliers

--- 25-flights.R
+++ 25-flights.R
@@ -1,5 +1,7 @@
+library(MASS)
 library(tidyverse)
-
+library(broom)
+library(ggrepel)
 
 # Read Data ---------------------------------------------------------------
 
@@ -26,23 +28,52 @@ dat %>%
   # map(~sum(is.na(.$passengers)))
   map(dim)
 
-tst <- dat %>%
-  select(loc_id, hub_type,
+# Subset - only hubs ------------------------------------------------------
+
+hubs <- dat %>%
+  dplyr::select(loc_id, hub_type,
          year, passengers, state) %>%
   spread(key = year, value = passengers) %>%
   filter(complete.cases(.),
-         hub_type != "Nonhub")
+         hub_type != "Nonhub") %>%
+  mutate(x = log(`2012`),
+         y = `2017`/`2012`)
 
+# model -------------------------------------------------------------------
+
+fit <- MASS::rlm(y ~ poly(x, 2), data = hubs)
+
+tidy(fit)
+
+hubs <- predict(fit,
+        newdata = hubs,
+        interval = "predict") %>%
+  as_tibble() %>% 
+  bind_cols(hubs)
 
 # plot --------------------------------------------------------------------
 
-tst %>%
-  ggplot(aes(x = `2012`,
-             y =`2017`/`2012`)) +
+hubs %>%
+  ggplot(aes(x = `2012`,# + `2017`/ 2,
+             y =`2017`/`2012`, 
+             ymin = lwr,
+             ymax = upr)) +
   geom_point() +
-  geom_point(data = tst %>% filter(state == "OH"),
-             colour = "blue") +
-  # geom_smooth() +
+  # geom_point(data = hubs %>% filter(state == "OH"),
+  #            colour = "blue") +
+  geom_text_repel(data = hubs %>%
+                    filter(y > upr | y < lwr),
+                  aes(label = loc_id)) +
+  stat_smooth(method = "rlm",
+              formula = y ~ poly(x, 2),
+              se = F) +
+  geom_ribbon(alpha = .3,
+              fill = "cyan3") +
   scale_x_log10() +
-  scale_y_log10() +
+  # scale_y_log10() +
+  # facet_wrap(facet = "state") +
   theme_bw()
+
+
+
+
