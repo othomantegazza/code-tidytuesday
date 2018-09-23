2019e75342fc95cf28c4db9b6204cd48cde0dacb
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-09-23 21:09:58 +0200

    Finalize

--- 25-flights.R
+++ 25-flights.R
@@ -36,7 +36,7 @@ hubs <- dat %>%
   spread(key = year, value = passengers) %>%
   filter(complete.cases(.),
          hub_type != "Nonhub") %>%
-  mutate(x = log(`2012`),
+  mutate(x = `2012`,
          y = `2017`/`2012`)
 
 # model -------------------------------------------------------------------
@@ -53,27 +53,46 @@ hubs <- predict(fit,
 
 # plot --------------------------------------------------------------------
 
+png(filename = "plots/25-airports.png",
+    height = 1500, width = 2500,
+    res = 300)
 hubs %>%
   ggplot(aes(x = `2012`,# + `2017`/ 2,
              y =`2017`/`2012`, 
              ymin = lwr,
              ymax = upr)) +
-  geom_point() +
+  geom_point(colour = "grey30") +
   # geom_point(data = hubs %>% filter(state == "OH"),
   #            colour = "blue") +
+  geom_point(data = hubs %>%
+               filter(y > upr | y < lwr),
+             aes(colour = state)) +
   geom_text_repel(data = hubs %>%
-                    filter(y > upr | y < lwr),
-                  aes(label = loc_id)) +
-  stat_smooth(method = "rlm",
-              formula = y ~ poly(x, 2),
-              se = F) +
+                    filter(y > upr),
+                  aes(label = loc_id), nudge_y = .1) +
+  geom_text_repel(data = hubs %>%
+                    filter(y < lwr),
+                  aes(label = loc_id), nudge_y = -.1) +
+  # stat_smooth(method = "rlm",
+  #             formula = y ~ poly(x, 2),
+  #             se = F) +
+  geom_line(aes(y = fit), lwd = 1.5,
+            alpha = .5) +
   geom_ribbon(alpha = .3,
               fill = "cyan3") +
   scale_x_log10() +
+  scale_y_continuous(breaks = c(.75, 1, 1.25, 1.5, 1.75, 2.0, 2.25),
+                     labels = paste(c("Decrease - ", "Same - ", "", "", "", "", "Increase - "),
+                     c(.75, 1, 1.25, 1.5, 1.75, 2.0, 2.25))) +
   # scale_y_log10() +
   # facet_wrap(facet = "state") +
-  theme_bw()
-
+  theme_bw() +
+  labs(title = "Passenger Increase in US Hubs (Airports)",
+       subtitle = "Between 2012 and 2017",
+       x = "Yearly Passengers in 2012 [log scale]",
+       y = "Passengers Increase in 2017 [2017 / 2012]",
+       caption = "Source: faa.gov, plot by @othomn")
+dev.off() 
 
 
 
