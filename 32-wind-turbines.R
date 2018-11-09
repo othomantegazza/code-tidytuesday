463b9e83cc3f2e445d48cfe5e285e7a00ca43938
Otho Mantegazza <otho.mantegazza@gmail.com>
2018-11-09 01:55:53 +0100

    Draw faceted geographical plot and tiled timecourse

--- 32-wind-turbines.R
+++ 32-wind-turbines.R
@@ -16,22 +16,74 @@ if(!file.exists(dat_file)) {
 } else {
   load(dat_file)
 }
+# Remove NA ---------------------------------------------------------------
 
-# explore patterns --------------------------------------------------------
+dat <- 
+  dat %>% 
+  select(p_year, p_cap, t_cap,
+         xlong, ylat) %>%
+  mutate(p_year = p_year %>%
+           na_if(-9999),
+         p_cap = p_cap %>%
+           na_if(-9999),
+         t_cap = t_cap %>% 
+           na_if(-9999))
+         
+dat_tidy <-
+  dat %>% 
+  filter(complete.cases(.)) %>% 
+  mutate(year_span = case_when(p_year >= 1981 & p_year <= 1993 ~ "1981 - 1993",
+                               p_year >= 1994 & p_year <= 2006 ~ "1994 - 2006",
+                               p_year >= 2007 & p_year <= 2018 ~ "2007 - 2018"))
 
-# REMEMBER TO REMOVE NA!!!!
+# explore geographical patterns--------------------------------------------------------
 
 usa_map <- map_data("state")
 
-dat %>%
+dat_tidy %>%
   ggplot(aes(x = xlong,
              y = ylat,
-             colour = p_cap)) +
-  geom_point(size = .2) +
-  geom_map(data=usa_map, map=usa_map,
-           aes(x=long, y=lat, map_id=region),
-           colour = "grey30", fill = NA, size = .2) +
+             colour = t_cap)) +
+  geom_point(size = .5, 
+             alpha = .3) +
+  geom_map(data=usa_map,
+           map=usa_map,
+           aes(x=long, y=lat,
+               map_id=region),
+           colour = "grey30", fill = NA,
+           size = .2) +
+  facet_grid(. ~ year_span) +
   coord_map(projection = "conic", lat = 40) +
-  lims(x = c(-125, -70),
+  lims(x = c(-119, -74),
        y = c(25, 50)) +
-  theme_minimal()
+  scale_color_viridis_c(guide = guide_colourbar(title.vjust = .7,
+                                                barwidth = 20,
+                                                barheight = .3)) +
+  theme_void() +
+  theme(legend.position = "bottom") +
+  labs(title = "Wind Turbine Installed in the US")
+
+
+# Explore time patterns ---------------------------------------------------
+
+library(ggridges)
+
+dat_tidy %>% 
+  ggplot(aes(x = t_cap,
+             y = p_year %>%
+               as.character() %>% 
+               as.factor() %>%
+               fct_rev())) +
+  # ggbeeswarm::geom_quasirandom() +
+  # scale_colour_viridis_c() 
+  geom_density_ridges(panel_scaling = FALSE)
+
+dat_tidy %>% 
+  ggplot(aes(x =  p_year,
+             y = t_cap)) +
+  # ggbeeswarm::geom_quasirandom(size = .2,
+  #                              alpha = .2)
+  stat_bin2d(binwidth = c(1, 500)) +
+  scale_fill_viridis_c() +
+  theme_bw()
+# dat %>% filter(t_cap > 5000) %>% pull(p_name)
