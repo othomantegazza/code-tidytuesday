6898531e7b099cef9d5363d25c98e1283515f8e7
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-01-10 15:23:41 +0100

    Improve plots

--- 2-02-tv-golden-age.R
+++ 2-02-tv-golden-age.R
@@ -1,26 +1,44 @@
 library(tidyverse)
 library(lubridate)
 library(rlang)
-library(gtable)
-library(gridExtra)
-library(grid)
-library(scico)
 library(magrittr)
+library(broom)
 
+
+# Set parameters ----------------------------------------------------------
+
+light_orange <- "orange"
+dark_orange <- "#E97E00"
+title_blue <- "#5F3BBA"
+highlight_blue <- "#46AEF9"
 
 # Set theme and functions -------------------------------------------------
 
-theme_set(theme_bw() + 
+theme_set(theme_gray() +
             theme(legend.position = "right",
                   text = element_text(family = "Arial Narrow",
-                                      colour = "grey40"),
-                  plot.title = element_text(colour = "grey20",
+                                      colour = "grey40",
+                                      size = 14),
+                  plot.title = element_text(colour = title_blue, # or  "#582F69"
                                             face = "bold",
-                                            size = 18, family = "Arial Narrow"),
+                                            size = 27,
+                                            family = "Arial Narrow"),
                   plot.subtitle = element_text(colour = "grey40",
                                                face = "bold",
                                                size = 12),
-                  aspect.ratio = .70))
+                  aspect.ratio = .67))
+
+
+save_plot <- function(p, file) 
+{
+  on.exit(
+    dev.off()
+  )
+  png(filename = file,
+      height = 1500, width = 2200,
+      res = 300)
+  p %>% print()
+}
 
 # Get Data ----------------------------------------------------------------
 
@@ -44,19 +62,27 @@ if(!file.exists(dat_path)) {
 
 # Basic plot, then add layer ----------------------------------------------
 
+dat_drama <- 
+  dat %>% 
+  filter(genres %>% str_detect("Drama"))
 
 p <- 
-dat %>%
-  filter(genres %>% str_detect("Drama")) %>% 
+  dat_drama %>% 
   ggplot(aes(x = date, y = av_rating, colour = share)) +
-  geom_point(alpha = .8) +
+  geom_point(alpha = .8, size = 2) +
   scale_color_viridis_c(trans = "log10",
-                        breaks = c(.01, .1, 1, 10, 50)
+                        breaks = c(.01, .1, 1, 10, 50),
                         # limits = c(NA, 100)
-                        ) +
+                        guide = guide_colourbar(barheight = 12)) +
   labs(x = "Original Airing Date",
        y = "Average Rating",
-       colour = str_wrap("Share of yearly votes", width = 12))
+       colour = str_wrap("Share of yearly votes", width = 12),
+       caption = "Data: IMDB | Plot by @othomn")
+
+
+save_plot(p = p +
+            labs(title = "Series with high rating get more votes"),
+          file = "plots/2-02-1-tv.png")
 
 # How to describe it?
 # 1. Is it normally distributed?
@@ -68,35 +94,73 @@ dat %>%
 
 #  Most of the high share ratings are above the regression line -----------
 
-p + labs(title = "Series with high rating get more votes")
+fit <- 
+  dat_drama %>% 
+  {lm(av_rating ~ poly(date, 3), data = .)}
+
+glance(fit)
+tidy(fit)
+fit %>% predict(newdata = dat_drama)
 
 p_lm <- 
   p +
   geom_smooth(method = "lm",
               formula = y ~ poly(x, 3),
-              colour = "orange")
+              colour = light_orange)
+
+save_plot(p = p_lm +
+            annotate(geom = "label",
+                     x = as_date("2013-10-01"), y = 8.5,
+                     label = "y ~ poly(x, 3)",
+                     fill = dark_orange,
+                     colour = "white",
+                     size = 4) +
+            labs(title = "Do ratings form trend over time? Not sure"),
+          file = "plots/2-02-2-tv.png")
 
 # Is Star Trek DS9 the only one below? ------------------------------------
 
 # base plot for highlightings
 
-
-p_lm +
+p_labs <- 
+  p_lm +
+  ggrepel::geom_label_repel(data = . %>% 
+                              mutate(pred_lm = fit %>% predict(newdata = dat_drama),
+                                     year = year(date)) %>% 
+                              group_by(year) %>% 
+                              top_n(n = 1, wt = share) %>% 
+                              # filter(av_rating < 8),
+                              filter(av_rating < pred_lm),
+                           aes(label = paste0(title,
+                                              ", season ",
+                                             seasonNumber) %>%
+                                 str_wrap(width = 20)),
+                           fontface = "bold",
+                           fill = dark_orange,
+                           angle = 90,
+                           colour = "white",
+                           segment.colour = dark_orange,
+                           box.padding = 0,
+                           ylim = c(NA, 6)) + 
   geom_point(data = . %>% 
                mutate(year = year(date)) %>% 
                group_by(year) %>% 
                top_n(n = 1, wt = share),
-             colour = "orange", size = 3) +
-  ggrepel::geom_text_repel(data = . %>% 
-                              mutate(year = year(date)) %>% 
-                              group_by(year) %>% 
-                              top_n(n = 1, wt = share) %>% 
-                              filter(av_rating < 8),
-                           aes(label = title), 
-                           angle = 90,
-                           colour = "orange",
-                           ylim = c(NA, 6))
+             shape = 21,
+             colour = title_blue,
+             fill = light_orange,
+             stroke = 1.6,
+             size = 3) 
+
   
+# p_labs
+
+save_plot(p = p_labs +
+            labs(title = str_wrap("Two Series have best yearly share of
+                 votes but low ratings")) +
+            theme(plot.title = element_text(size = 21)), 
+          file = "plots/2-02-3-tv.png")
+
 
 
 tst <- 
