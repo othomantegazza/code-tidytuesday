fcf48725449d8a21ad358bf056658a15088ae9b5
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-02-01 18:53:47 +0100

    timeline with percentage change

--- 2-05-milk.R
+++ 2-05-milk.R
@@ -1,12 +1,8 @@
 library(tidyverse)
-library(lubridate)
-library(rlang)
-library(gtable)
-library(gridExtra)
-library(grid)
 library(scico)
+library(tibbletime)
 
-# Get Data ----------------------------------------------------------------
+# Get Data milk cow -------------------------------------------------------
 
 
 dat_path <- "data/2-05-milkcow-facts.Rdata"
@@ -26,14 +22,7 @@ if(!file.exists(dat_path)) {
 }
 
 
-# Explore -----------------------------------------------------------------
-
-library(GGally)
-
-ggpairs(dat_milkcow)
-
-
-# Clean chese -------------------------------------------------------------
+# Get data Clean chese -----------------------------------------------------
 
 dat_path <- "data/2-05-clean-cheese.Rdata"
 dat_url <- paste0("https://raw.githubusercontent.com/",
@@ -51,19 +40,7 @@ if(!file.exists(dat_path)) {
   load(dat_path)
 }
 
-
-# explore -----------------------------------------------------------------
-
-# dat %>% 
-#   gather(Cheddar:Blue,
-#          key = "chees_type",  value = "lbs") %>% 
-#   ggplot(aes(x = Year, 
-#              y = lbs,
-#              fill = chees_type)) +
-#   geom_bar(stat = "identity")
-
-
-# Milk products -------------------------------------------------------------
+# Get data Milk products ---------------------------------------------------
 
 dat_path <- "data/2-05-milk-product-facts.Rdata"
 dat_url <- paste0("https://raw.githubusercontent.com/",
@@ -81,102 +58,150 @@ if(!file.exists(dat_path)) {
 }
 
 
-# explore -----------------------------------------------------------------
+# Percent density ---------------------------------------------------------
 
-# dat %>% 
-#   gather(fluid_milk:fluid_yogurt,
-#          key = "type",  value = "lbs") %>% 
-#   ggplot(aes(x = year, 
-#              y = lbs,
-#              fill = type)) +
-#   geom_bar(stat = "identity")
+# roll over tibbles
+roll_percent <- rollify(.f = function(n) (n[2] - n[1])*100/n[1], 2)
 
 
-# explore all -------------------------------------------------------------
+# With interesting patterns
+up <- c("butter", #"fluid_yogurt", 
+        "mozzarella", "total american cheese")
+down <- "fluid_milk"
 
+keep <- c(down, up)
+
+# why are those two dataset separated?
 dat <- 
   dat_cheese %>%
   full_join(dat_milkprods, by = "year") %>%
-  gather(cheddar:dry_whey, key = "type", value = "lbs")
-  
+  gather(cheddar:dry_whey,
+         key = "type", value = "lbs") %>% 
+  filter(complete.cases(.)) %>% 
+  filter(type %in% keep)
 
-dat_dry <- 
+dat_perc <-  
   dat %>%
-  filter(str_detect(type, "dry")) %>% 
-  group_by(year) %>% 
-  summarise(lbs = sum(lbs)) %>% 
-  mutate(type = "dry")
-
-
-dat_evap <- 
-  dat %>%
-  filter(str_detect(type, "evap")) %>% 
-  group_by(year) %>% 
-  summarise(lbs = sum(lbs)) %>% 
-  mutate(type = "evap")
-
-
-dat_short <- 
-  dat %>%
-  filter(!str_detect(type, "dry"),
-         !str_detect(type, "evap")) %>% 
-  bind_rows(dat_dry) %>% 
-  bind_rows(dat_evap)
-  
-
-dat %>%  
-  ggplot(aes(x = year,
-             y = lbs)) +
-  geom_line() +
-  facet_wrap("type", scales = "free")
-
-dat_short %>%  
-  ggplot(aes(x = year,
-             y = lbs)) +
-  geom_line() +
-  facet_wrap("type", scales = "free")
-
-
-up <- c("butter", "fluid_yogurt", "italian other", 
-        "mozzarella", "total american cheese")
-down <- c("fluid_milk", "evap")
-
-dat_short %>% 
-  filter(type %in% down) %>% 
-  ggplot(aes(x = year,
-             y = lbs,
-             fill = type)) +
-  geom_bar(stat = "identity", width = 1, alpha = .5)
-
-dat_short %>% 
-  filter(type %in% c(up, down)) %>% 
-  ggplot(aes(x = year,
-             y = lbs,
-             fill = type)) +
-  geom_bar(stat = "identity", width = 1, alpha = .5)
-
-library(tibbletime)
-
-roll_percent <- rollify(.f = function(n) (n[2] - n[1])*100/n[1], 2)
-
-dat_perc <- 
-  dat_milkprods %>% 
-  select(year, fluid_milk) %>% 
-  mutate(percent = roll_percent(fluid_milk)) %>% 
+  group_by(type) %>%
+  mutate(percent = roll_percent(lbs)) %>% 
+  ungroup() %>% 
   filter(complete.cases(.)) 
 
-dens <-  
-  dat_perc$percent %>% 
-  density(n = 2^12) %>% 
-  {tibble(x = .$x,
-          y = .$y)}
+# how do I prepare a density dataset?
+get_density <- function(type_of_cheese) 
+{
+  dat_perc %>% 
+    filter(type == type_of_cheese) %>% 
+    pull(percent) %>% 
+    density(n = 2^12) %>% 
+    {tibble(type = type_of_cheese,
+            x = .$x,
+            y = .$y)}
+}
+
+
+dens <- 
+  dat_perc$type %>% 
+  unique() %>% 
+  map_df(get_density)
+
+# center the divergent fill to 0
+fill_lim <- dens$x %>% range() %>% abs() %>% max()
 
 dens %>% 
   ggplot(aes(x = x,
              y = y,
              xend = x)) +
   geom_segment(aes(colour = x),yend = 0) +
-  geom_line(color = "grey30", size = 2) +
+  geom_line(color = "grey30", size = 1.5) +
+  facet_wrap(facets = "type", 
+             ncol = 1, scales = "free_y") +
   # scale_color_viridis_c(option = "E")
-  scico::scale_color_scico(palette = "roma") +
-  theme_minimal()
+  scale_color_scico(palette = "roma",
+                    direction = 1,
+                    limits =  c(-fill_lim, fill_lim)) +
+  theme_grey() 
+
+
+dat_perc %>% 
+  filter
+
+dat_perc %>% 
+  ggplot(aes(x = year,
+             y = percent,
+             xend = year)) +
+  geom_line() +
+  geom_segment(aes(colour = percent),
+               yend = 0) +
+  # geom_area(aes(fill = percent)) +
+  facet_grid(type ~ ., scales = "free_y")
+
+dat_perc %>% 
+  ggplot(aes(x = year,
+             y = percent,
+             colour = percent)) +
+  geom_smooth(aes(colour = ..y..),
+              se = FALSE) +
+  # geom_line() +
+  geom_point() +
+  scale_color_scico(palette = "roma",
+                    direction = 1,
+                    limits =  c(-fill_lim, fill_lim)) +
+  # geom_area(aes(fill = percent)) +
+  facet_grid(type ~ ., scales = "free_y") +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "grey20"))
+
+scico_roma <- scico(2, palette = "roma")
+
+dat_perc %>% 
+  ggplot(aes(x = year,
+             y = percent,
+             fill = percent)) +
+  # geom_smooth(aes(colour = ..y..),
+  #             se = FALSE) +
+  # geom_line() +
+  # geom_point() +
+  geom_bar(stat = "identity") +
+  scale_fill_scico(palette = "roma",
+                   direction = 1,
+                   limits =  c(-fill_lim, fill_lim)) +
+  # scale_fill_gradient2(low = scico_roma[1],
+  #                      high = scico_roma[2],
+  #                      midpoint = 0) +
+  # geom_area(aes(fill = percent)) +
+  facet_wrap(facets = "type",
+             # scales = "free_y",
+             ncol = 1) +
+  theme_void() +
+  theme(#plot.background = element_rect(fill = "grey20"),
+        panel.background = element_rect(fill = "grey20"),
+        panel.grid = element_blank())
+
+
+dat_perc %>% 
+  filter(type == "butter") %>% 
+  mutate(yend = lbs + (percent/10)) %>% 
+  ggplot(aes(x = year,
+             y = lbs)) +
+  # geom_line(color = "grey80") +
+  geom_segment(aes(yend = yend,
+                   xend = ..x..,
+                   colour = percent),
+               size = 2,
+               arrow = arrow(length = unit(1.2, "mm"),
+                             type = "closed")) +
+  geom_point(colour = "grey40", size = 2) +
+  geom_text(aes(y = case_when(percent > 0 ~ yend + .12,
+                              TRUE ~ yend - .12),
+                label = percent %>% 
+                  round() %>% paste0("%"),
+                colour = percent),
+            size = 3) +
+  scale_colour_scico(palette = "roma",
+                   direction = 1,
+                   limits =  c(-fill_lim, fill_lim),
+                   guide = FALSE) +
+  guides(colour = element_blank()) +
+  theme_bw()
+ 
