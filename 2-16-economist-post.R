b4ca432a08908121a0f327bc33082c97f7b1f0d7
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-04-16 20:39:10 +0200

    place plots in grid

--- 2-16-economist-post.R
+++ 2-16-economist-post.R
@@ -50,9 +50,10 @@ plot_circle <- function(percent_women) {
     geom_text(data = . %>% 
                 filter(gender == "female"),
               aes(x = -.5, y = 0,
-                  label = value,
+                  label = paste0(".", value*100),
                   colour = value),
-              size = 10) +
+              size = 10,
+              fontface = "bold") +
     coord_polar(theta = "y") +
     scale_fill_viridis_d(begin = .1, end = .9, guide = FALSE) +
     scale_colour_viridis_c(guide = FALSE, limits = c(0, 1)) +
@@ -62,19 +63,54 @@ plot_circle <- function(percent_women) {
   return(p)
 }
 
-p_list <- 
+dat_plots <- 
   dat %>% 
-  mutate(percent_women = as.numeric(percent_women)) %>% 
-  pull(percent_women) %>% 
-  map(plot_circle)
+  mutate(percent_women = as.numeric(percent_women)) %>%
+  arrange(country, field) %>% 
+  mutate(plots = percent_women %>% map(plot_circle))
 
-p_list[[40]]
-
+dat_plots %>% pull(plots) %>% .[[1]]
 
 # define grid -------------------------------------------------------------
 
 s_margin <- .1
 u_margin <- .2
 
-
 length(p_list)
+# 60
+
+y_n <- dat_plots %>% pull(country) %>% unique() %>% length()
+# 12
+
+x_n <- dat_plots %>% pull(field) %>% unique() %>% length()
+# 5
+
+# a grid 12 x 5
+
+width <- (1 - 2 * s_margin) / x_n
+height <- (1 - 2*u_margin) / y_n
+
+# plot on grid ------------------------------------------------------------
+
+plot_to_vp <- function(p, x, y, width, height) {
+  print(p,
+        vp = viewport(x = x, y = y, width = width, height = height))
+  return(NULL)
+}
+
+grid.newpage()
+
+
+svglite::svglite(file = "plots/2-16-economist-post.svg",
+                 height = 32,
+                 width = 15)
+dat_plots %>% 
+  mutate(x = seq(s_margin, 1 - s_margin, length.out = x_n + 1)[1:x_n] %>% rep(y_n),
+         y = seq(1 - u_margin, u_margin, length.out = y_n) %>% rep(each = x_n)) %>% 
+  transmute(p = .data$plots,
+            x = .data$x,
+            y = .data$y,
+            width = width,
+            height = height) %>% 
+  pmap(plot_to_vp)
+dev.off()
