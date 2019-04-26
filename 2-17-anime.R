af09beb3e69e325567044ef9df5c8aa151d9dac2
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-04-26 10:32:19 +0200

    define graph layout

--- 2-17-anime.R
+++ 2-17-anime.R
@@ -56,22 +56,89 @@ vs <-
   dat_graph %>%
   filter(x == y) %>% 
   select(genre = x,
-         count)
+         count) %>% 
+  filter(count >= 500)
 
 edges <- 
   dat_graph %>% 
+  mutate(n_x = case_when(x == y ~ count)) %>% 
+  arrange(x, n_x) %>% 
+  fill(n_x) %>% 
+  mutate(perc = count/n_x) %>% 
+  filter(perc > .2,
+         x %in% vs$genre,
+         y %in% vs$genre) %>% 
   filter(x != y)
 
-g <- graph_from_data_frame(d = edges)
+g <- graph_from_data_frame(d = edges, vertices = vs)
 
 
 # plot with ggraph --------------------------------------------------------
 
 library(ggraph)
 
-# g %>% as_long_data_frame()
+
+g %>% as_long_data_frame()
+
+set.seed(7)
+# set.seed(12)
+# set.seed(16)
+p <- 
+  g %>% 
+  ggraph(layout = "graphopt") +
+  # ggraph(layout = "lgl") +
+  geom_edge_link(aes(alpha = count),
+                 colour = "#4C63C3") +
+  geom_node_point(aes(size = count), colour = "#B63A82") +
+  geom_node_text(aes(label = name),
+                 nudge_x = 1,
+                 nudge_y = 1,
+                 vjust = 0,
+                 hjust = 0) +
+  # geom_text(aes(label = count)) +
+  expand_limits(x = c(0, 50)) +
+  theme_void()
+
+p %>% ggplotGrob()
+
+p +
+  guides(alpha = guide_legend(nrow = 1),
+         size = guide_legend(nrow = 1)) +
+  theme(legend.position = c(.7, .3))
+
+# Save --------------------------------------------------------------------
+
+png(filename = "plots/2-17-anime.png", 
+    height = 7,
+    width = 10,
+    units = "in",
+    res = 300)
+p
+dev.off()
+
+# others ------------------------------------------------------------------
+
+
+
+g %>% 
+  ggraph(layout = "linear", circular = TRUE) +
+  geom_edge_arc(aes(alpha = count),
+                 colour = "#4C63C3") +
+  geom_node_point(aes(size = count), colour = "#B63A82") +
+  geom_node_text(aes(label = name),
+                 angle = 90) +
+  # geom_text(aes(label = count)) +
+  theme_void()
 
 g %>% 
   ggraph(layout = "linear") +
   geom_edge_arc(aes(alpha = count)) +
   theme_void()
+
+g %>% 
+  ggraph(layout = "linear", circular = TRUE) +
+  geom_edge_arc2(aes(alpha = count)) +
+  theme_void()
+
+# c('star', 'circle', 'gem', 'dh', 'graphopt', 'grid', 'mds', 
+#   'randomly', 'fr', 'kk', 'drl', 'lgl')
