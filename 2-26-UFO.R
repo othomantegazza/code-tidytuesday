2725565cfce899caeab10063bc4b37d27374521f
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-06-25 18:32:09 +0200

    draft plot

--- 2-26-UFO.R
+++ 2-26-UFO.R
@@ -1,5 +1,21 @@
 library(tidyverse)
+library(jsonlite)
 library(lubridate)
+library(grid)
+library(ggrepel)
+library(ggforce)
+
+purple <- "#AA2255"
+purple2 <- "#BB2255"
+blue <-  "#4C63C3"
+mid <- "#D5A080"
+mid2 <- "#E2CD92"
+grey <- "grey30"
+bg_col <- "#F0F0CB" # "#EAEA9F"
+
+cp <- colorRamp(colors = c(bg_col, purple2))
+
+rgb(cp(.1), maxColorValue = 255)
 
 # read data ---------------------------------------------------------------
 
@@ -18,6 +34,11 @@ if(!file.exists(data_path)) {
   load(data_path)
 }
 
+# us towns
+
+us_towns <- fromJSON("https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json")
+
+save(us_towns, file = "data/2-26-ufo-us-towns.Rdata")
 
 # explore -----------------------------------------------------------------
 
@@ -44,9 +65,54 @@ ufo %>%
   scale_y_log10() +
   theme_bw()
 
-ufo %>%
+
+# plot --------------------------------------------------------------------
+
+towns_in <- 
+  tibble(city = c("New York", "Los Angeles", "Seattle", "Phoenix"),
+         nudge_x = c(4, -4, -4, -2),
+         nudge_y = c(-2, -2, -2, -2),
+         vjust = case_when(nudge_y > 0 ~ 0, TRUE ~ 1),
+         hjust = case_when(nudge_x > 0 ~ 0, TRUE ~ 1)) %>% 
+  left_join(us_towns, by = "city")
+
+p <- 
+  ufo %>%
   ggplot(aes(x = longitude,
-                   y = latitude)) +
-  geom_hex(colour = "grey", size = .2, bins = c(100, 120)) +
-  lims(x = c(-120, -50),
-       y = c(25, 50))
+             y = latitude)) +
+  geom_hex(colour = bg_col, size = .6, bins = c(100, 150)) +
+  geom_link(data = towns_in,
+               aes(xend = longitude + nudge_x,
+                   yend = latitude + nudge_y,
+            colour = case_when(..index.. > .12 & ..index.. < .93 ~ "grey",
+                                # ~ "grey",
+                               TRUE ~ "none")),
+            size = .4) +
+  geom_text(data = towns_in,
+            aes(label = city,
+                x = longitude + nudge_x,
+                y = latitude + nudge_y,
+                hjust = hjust,
+                vjust = vjust),
+            colour = grey,
+            size = 2.6) +
+  lims(x = c(-128, -50),
+       y = c(25, 49)) +
+  scale_fill_gradient(low = mid2,
+                      # trans = "log",
+                      high = purple) +
+  scale_colour_manual(values = c(grey = grey, none = "#FFFFFF00")) +
+  guides(colour = FALSE) +
+  theme_void() +
+  theme(aspect.ratio = .4) 
+ 
+# grid.rect(gp = gpar(fill = bg_col))
+# print(p, vp = viewport())
+
+png(filename = "plots/2-26-ufo.png",
+    height = 1200, 
+    width = 3000,
+    res = 400)
+grid.rect(gp = gpar(fill = bg_col))
+print(p, vp = viewport())
+dev.off()
