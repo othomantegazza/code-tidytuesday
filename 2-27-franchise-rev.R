cfddd69690e38baa5caee9e4d4f38a5eedee70ec
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-07-02 23:56:36 +0200

    finalize plot

--- 2-27-franchise-rev.R
+++ 2-27-franchise-rev.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(packcircles) ## easily package circles
 library(ggforce)
+library(grid)
+
+bg_col <- "#F6F6DF" # "#E4F3F9"
 
 # get data ----------------------------------------------------------------
 
@@ -20,7 +23,6 @@ if(!file.exists(data_path)) {
 } else {
   load(data_path)
 }
-
 
 # explore -----------------------------------------------------------------
 
@@ -89,7 +91,7 @@ packs_df %>%
 
 # inner circles -----------------------------------------------------------
 
-# 1. rank categories and estimate revenue ratios
+# 1. rank categories and estimate revenue ratios --------------------------
 
 fr_ranked <- 
   fr %>% 
@@ -102,12 +104,24 @@ fr_ranked <-
   # cummulative revenue from lowest
   mutate(cumul_ratio = cumsum(rev_ratio))
 
-# 2. set colors
-colorz <- 
-  c("#D9F6FF","#E97E00","#E0E0D2","#5867A6","#100089","#1E2D6C","#CD0A62","#CB7BA5") %>% 
+# 2. set colors -----------------------------------------------------------
+colorz <-
+  c("#D9F6FF", # "#EAEA9F", 
+    "#E97E00","#E0E0D2","#5867A6", # "#44D4FF", "#E2CD92",
+    "#27A6D3", "#1E2D6C","#CD0A62","#CB7BA5") %>%
   set_names(nm = fr %>% pull(revenue_category) %>% unique())
 
-# 3. try plot first rank 
+# colorz <- 
+#   Redmonder::redmonder.pal(n = 8, name = "qMSOPu2")[2:8] %>% 
+#   c("#27A6D3") %>% 
+#   set_names(nm = fr %>% pull(revenue_category) %>% unique())
+
+
+# colorz <-
+#   c("#D81B60","#8E24AA","#5E35B1","#3949AB","#1E88E5","#039BE5","#00ACC1","#00897B") %>%
+#   set_names(nm = fr %>% pull(revenue_category) %>% unique())
+
+# 3. try plot first rank --------------------------------------------
 #  with ggforce geom_circle
 
 pack1 <- 
@@ -126,9 +140,15 @@ p <-
   scale_fill_manual(values = colorz) +
   scale_colour_manual(values = colorz) +
   coord_equal() +
-  theme_void()
+  theme_void() +
+  theme(legend.position = "right",
+        legend.justification = "center") +
+  guides(colour = FALSE,
+         fill = FALSE) 
 
-# 4. make a list of ranks and loop over it
+p
+
+# 4. make a list of ranks and loop over it -----------------------------------
 
 get_rank_circles <- function(rank_in = 2) {
   packs_n <- 
@@ -153,9 +173,60 @@ for(i in circles_dfs) {
                 size = .1)
 } 
 
+
+# 5. make legend maually --------------------------------------------------
+
+leg <- 
+  tibble(y = rep(c(1,2), each = 4),
+         x = rep(1:4, times = 2),
+         fill = colorz %>% names(),
+         r = .1) %>% 
+  ggplot() +
+  geom_text(aes(x = x, y = y - .26,
+                label = str_wrap(fill, 15)),
+            lineheight = .9,
+            size = 1.2,
+            vjust = 1, family = "courier", fontface = "bold") +
+  geom_circle(aes(x0 = x,
+                  y0 = y,
+                  fill = fill,
+                  colour = fill,
+                  r = r)) +
+  guides(fill = FALSE, colour = FALSE) +
+  scale_fill_manual(values = colorz) +
+  scale_colour_manual(values = colorz) +
+  coord_equal() +
+  theme_void() +
+  lims(y = c(0.5, 2.3),
+       x = c(.5, 4.5))
+
+leg
+
+# 6. save plot ------------------------------------------------------------
+
+
+
 png(filename = "plots/2-27-franchise-rev.png",
-    height = 1300,
-    width = 2000,
+    height = 1600,
+    width = 2700,
     res = 400)
-p
+grid.newpage()
+grid.rect(gp = gpar(fill = bg_col)) # background
+p %>% print(vp = viewport(x = .42, y = .5)) # plot
+leg %>% print(vp = viewport(x = .83, y = .16, width = .26)) # legend
+grid.text(label = "Media Franchise Revenues", # title
+          x = .98,
+          y = .3,
+          hjust = 1,
+          gp = gpar(fontfamily = "courier",
+                    col = "#100089",
+                    fontsize = 10.5,
+                    fontface = "bold"))
+grid.text(label = "Data by Wikipedia | Plot by @othomn", # caption
+          x = .02, y = .03, 
+          hjust = 0,
+          gp = gpar(fontfamily = "courier",
+                    col = "#AA2255",
+                    # fontface = "bold",
+                    fontsize = 4))
 dev.off()
