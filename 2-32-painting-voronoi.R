0971669a6aa090b02ff5e6196afa33e0b79eb7bf
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-08-07 16:26:08 +0200

    clean and comment code

--- 2-32-painting-voronoi.R
+++ 2-32-painting-voronoi.R
@@ -1,12 +1,19 @@
 # Most steps are taken form:
 # https://chichacha.netlify.com/2018/11/12/utilizing-k-means-to-extract-colours-from-your-favourite-images/
 
+
+# set up ------------------------------------------------------------------
+
+
 library(tidyverse)
 library(imager)
 library(ggvoronoi)
 library(grid)
+
+# background color
 bg_color <- "#E8EDEF"
 
+# Get image ---------------------------------------------------------------
 
 image_path <- "data/2-32-bob-ross-sunset.Rdata"
 
@@ -19,7 +26,8 @@ if(!file.exists(image_path)) {
   load(image_path)
 }
 
-# plot(img)
+
+# analyze -----------------------------------------------------------------
 
 # number of pixel?
 dim(img)[1]*dim(img)[2]
@@ -41,19 +49,22 @@ hex_pix <-
   hex_pix %>%
   inner_join(grey_pix)
 
-# p <- ggplot(hex_pix,aes(x,y))+geom_raster(aes(fill=hexval))+scale_fill_identity()
-# p
+
+# sample pixels ------------------------------------------------------------
 
 set.seed(63); hex_pix_mini <- 
   hex_pix %>% 
-  sample_n(2500, weight = value) 
+  sample_n(2500, weight = value) # more likely if luminosity is higher 
   
+# colors named vectors
+# for plotting
 pix_colors <- 
   hex_pix_mini %>% 
   pull(hexval) %>% 
   {purrr::set_names(x = .,
                    nm = .)}
 
+# range of axis
 range_x <- c(0, dim(img)[1])
 range_y = c(dim(img)[2], 0)
 
@@ -79,31 +90,41 @@ p <-
 # p %>% print()
 # dev.off()
 
+
+# decorate plot with grid and save ----------------------------------------
+
 # png parameters
 img_height <- 2800
 img_width <- 2300
 
-# position of top left corner
+# position of bottom left corner
 img_x <- .2
 img_y <- .18
+
+# and plot size
 plot_width <- 1 - img_x - .05
 plot_height <- 1 - img_y - .05
 
+# save
 png(file = "plots/2-32-painting-voronoi.png",
     height = img_height,
     width = img_width,
     res = 300)
 grid.newpage()
+# background
 grid.rect(gp = gpar(fill = "#838798"))
+# plot
 p %>% print(vp = viewport(x = img_x, y = img_y, 
                           just = c(0, 0),
                           height = plot_height,
                           width = plot_width))
+# side caption
 grid.text(label = str_wrap("Voronoi tesselation of one of Bob Ross paintigs. Inspired by @chisatini's blog.",
                            width = 14),
           x = img_x - .003, y = .945,
           hjust = 1, vjust = 1, gp = gpar(size = 14, lineheight = 1,
                                           col = bg_color))
+# signature
 grid.text(label = "Painting by Bob Ross | Plot by @othomn",
           x = .92, y = .1,
           hjust = 1, vjust = 1, gp = gpar(fontsize = 10, lineheight = 1,
