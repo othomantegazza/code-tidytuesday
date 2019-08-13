754d3d88b2f1c6f749d02d1913bf7bacb0a05c30
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-08-14 00:30:52 +0200

    add delaunay tiles to plot function

--- 2-32-painting-voronoi-extra.R
+++ 2-32-painting-voronoi-extra.R
@@ -14,7 +14,9 @@ img_to_voronoi <- function(img,
                            border_size = .2,
                            pick_by = NULL,
                            bg_color = "#E8EDEF",
-                           delaunay = FALSE) {
+                           delaunay = FALSE,
+                           delaunay_tile = FALSE,
+                           grays = TRUE) {
   
   pick_by <- enquo(pick_by)
 
@@ -25,9 +27,8 @@ img_to_voronoi <- function(img,
     mutate(hexval = rgb(c.1,c.2,c.3))
   
 
-
   # luminosity for every pixes
-  grey_pix <- 
+  if(grays) {grey_pix <- 
     img %>% 
     grayscale() %>% 
     as.data.frame()
@@ -35,7 +36,7 @@ img_to_voronoi <- function(img,
   # merge
   hex_pix <- 
     hex_pix %>%
-    inner_join(grey_pix)
+    inner_join(grey_pix)}
   
   # p <- ggplot(hex_pix,aes(x,y))+geom_raster(aes(fill=hexval))+scale_fill_identity()
   # p
@@ -60,14 +61,28 @@ img_to_voronoi <- function(img,
     ggplot(aes(x = x,
                y = y)) +
     # geom_point(aes(colour = hexvalue)) +
-    ggvoronoi::geom_voronoi(aes(fill = hexval),
+    {if(!delaunay_tile) {
+      ggvoronoi::geom_voronoi(aes(fill = hexval),
                             size = border_size,
-                            colour = bg_color) +
-    {if(delaunay) ggforce::geom_delaunay_segment(colour = "grey80",
+                            colour = bg_color)
+      }} +
+    {if(delaunay) {
+      ggforce::geom_delaunay_segment(colour = "grey80",
                                                  size = .2,
-                                                 linetype = 2)} +
+                                                 linetype = 2)
+      }} +
+    {if(delaunay_tile) {
+      # ggforce::geom_delaunay_tile(
+      #   # aes(fill = hexval),
+      #   colour = "white")
+      ggforce::geom_delaunay_segment2(aes(colour = hexval,
+                                          group = -1),
+                                      size = border_size,
+                                      lineend = "round")
+      }} +
     scale_y_reverse(limits = range_y) +
     scale_fill_manual(values = pix_colors, guide = FALSE) +
+    {if(delaunay_tile) scale_colour_manual(values = pix_colors, guide = FALSE)} +
     coord_fixed() +
     theme_void() +
     lims(x = range_x)
@@ -152,3 +167,89 @@ grid.newpage()
 grid.rect(gp = gpar(fill = "#6A5A77"))
 p %>% print(vp = viewport())
 dev.off()
+
+
+# img 4 -------------------------------------------------------------------
+
+img <- load.image("~/Downloads/img4.jpeg")
+
+
+p <- img_to_voronoi(img = img,
+                    n_pix = 5000,
+                    # pick_by = 1/(value + .0000001),
+                    pick_by = (( c.1 * ( c.3 )^2)/(c.2 + 0.000001)),
+                    border_size = .2,
+                    bg_color = "#ffffff",
+                    delaunay = F)
+
+p
+
+p <- img_to_voronoi(img = img,
+                    n_pix = 1000,
+                    # pick_by = 1/(value + .0000001),
+                    pick_by = c.1,
+                    border_size = .2,
+                    bg_color = "#ffffff",
+                    delaunay = F)
+
+png("plots/2-32-img4-voronoi.png",
+    width = 1700,
+    height = 2400,
+    res = 300)
+grid.newpage()
+grid.rect(gp = gpar(fill = "#ffffff"))
+p %>% print(vp = viewport())
+dev.off()
+
+
+# img 5 -------------------------------------------------------------------
+
+img <- load.image("~/Downloads/img5.png")
+
+p <- img_to_voronoi(img = img,
+                    n_pix = 5000,
+                    # pick_by = 1/(value + .0000001),
+                    pick_by = c.1,
+                    border_size = .2,
+                    bg_color = "#ffffff",
+                    delaunay = F,
+                    delaunay_tile = F,
+                    grays = F)
+
+# p
+
+png("plots/2-32-img5-voronoi.png",
+    width = 1700,
+    height = 2200,
+    res = 300)
+grid.newpage()
+grid.rect(gp = gpar(fill = "#CD0A39"))
+p %>% print(vp = viewport())
+dev.off()
+
+
+p_del <- img_to_voronoi(img = img,
+                    n_pix = 1000,
+                    # pick_by = 1/(value + .0000001),
+                    pick_by = c.1,
+                    border_size = 1.8,
+                    bg_color = "#ffffff",
+                    delaunay = F,
+                    delaunay_tile = T,
+                    grays = F)
+
+p_del
+
+
+png("plots/2-32-img5-delaunay.png",
+    width = 1700,
+    height = 2200,
+    res = 300)
+grid.newpage()
+grid.rect(gp = gpar(fill = "#CD0A39"))
+p_del %>% print(vp = viewport())
+dev.off()
+
+# ggplot(iris, aes(Sepal.Length, Sepal.Width)) +
+#   ggforce::geom_delaunay_segment2(aes(colour = Species, group = -1), size = 2,
+#                          lineend = 'round')
