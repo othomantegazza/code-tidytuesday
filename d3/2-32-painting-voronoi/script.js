19b3a28b3120c2ea750e824c721931313fd9562a
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-08-14 12:06:07 +0200

    adapt for gist

--- d3/2-32-painting-voronoi/script.js
+++ d3/2-32-painting-voronoi/script.js
@@ -27,47 +27,13 @@ var svg = d3.select("#my_dataviz")
 
 // X axis: scale and draw:
 var x = d3.scaleLinear()
-    .domain([0, 450])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+    .domain([0, 450])   
     .range([0, width]);
-
-/*
-get max of array  column with:
-    d3.max(hexpix, d => d.x)
-*/
-
-/* svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x)); */
 
 // Y axis: generate scale and draw
 var y = d3.scaleLinear()
          .domain([0, 600])
          .range([0, height]);
-
-/* svg.append("g")
-    .call(d3.axisLeft(y)); */
-    
-
-
-// Draw the points --------------------------------------------------
-
-
-//Initiate a group element for the circles	
-var circleGroup = svg.append("g")
-                        .attr("class", "circleWrapper"); 
-                        
-/*circleGroup.selectAll()
-              .data(hexpix)
-              .enter().append("circle")
-                .attr("cx", d => x(d.x))
-                .attr("cy", d => y(d.y))
-                .attr("r", "3px")
-                .style("fill", d => d.hexval)
-              /* .transition().duration(10000)
-                .attr("r", "10px")
-                .style("opacity", ".5") ;*/
-
-
 
 
 // Draw the Voronoi grid ---------------------------------------------
@@ -104,57 +70,7 @@ var site = svg.append("g")
       .duration(9000)
       .attr("r", getMaxLinkDistance(voronoiTess.links(hexpix)));               
 
-
-/* voronoiGroup.selectAll()
-              .data(voronoiTess(hexpix).polygons())
-              .enter().append("path")
-                .attr("d", function(d, i) { return "M" + d.join("L") + "Z"; })
-                .attr("class", function(d, i) { return "pos" + [d.data.x] + [d.data.y]; })
-              .datum(function(d, i) { return d.data; })
-                .style("stroke", "#FFF") 
-                .style("stroke-width", .5)
-                //.style("opacity", .5)
-                .style("fill", d => d.hexval)
-                .attr("transform", function(d) { var xin = x(d.x)
-                                                  yin = y(d.y);
-
-                                               return "translate(" + xin + "," + yin + ")"
-                                                       + "scale(" + scale + ")"
-                                                       + "translate(" + -xin + "," + -yin + ")";
-                                              })
-              .transition().duration(7000)
-              .attr("transform", function(d) { var xin = x(d.x)
-                                                yin = y(d.y);
-
-                                             return "translate(" + xin + "," + yin + ")"
-                                                     + "scale(" + 1 + ")"
-                                                     + "translate(" + -xin + "," + -yin + ")";
-                                            }); */
-
-
-
-
-/* 
-.on("mouseover", enlarge) */
-
-/* .on("mouseover", function(d) { return d.style("opacity", 1); }) */
-
-// Function on hover --------------------------------------------------
-
-function enlarge(tile) {
-  var cls = "pos" + [tile.x] + [tile.y]
-
-  console.log(cls)
-
-  var element = d3.select("." + cls)
-
-  console.log(element)
-  
-  element.style("opacity", 1)
-}
-
-
-// get max voronoi dist to center
+// get max voronoi dist to center ----------------------------------------
 
 function getMaxLinkDistance(links) {
   return d3.max(links.map(function(d) {
