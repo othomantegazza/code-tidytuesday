c22a7ac66f1471571f71a6e54d87565fdd2651d4
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-08-16 15:59:54 +0200

    clean code

--- 3-33-rome.R
+++ 3-33-rome.R
@@ -54,7 +54,6 @@ bg_from_template <- "#434343"
 emp_color <- 
   emps_simple %>% 
   {set_names(nm = .$name,
-             # x = sample(ochRe::ochre_palettes$olsen_seq, size = nrow(.), replace = T))}
              x = rep(colors_from_template, length.out = nrow(.)))}
 
 # select color
@@ -89,9 +88,7 @@ emps_toplot <-
 
 height  <- 10; width <- 30
 margin_left <- .3; margin_right <- .03; margin_side <- margin_left + margin_right
-# n_long <- 10
-# by_long <- 10
-# rect_margin <- .4
+
 
 # size of block of ten years
 block_size <- (1 - margin_side)/10
@@ -123,9 +120,6 @@ bar_height <- .08
 
 bar_gap <- bar_height*.1
 
-
-
-
 # make tibble with rectangle shapes --------------------------------------
 
 
@@ -137,13 +131,6 @@ shapes_basic <-
          height = bar_height,
          gp = map(name, make_gpar),
          vjust = 1) 
-  # transition years
-  # mutate(last_year = case_when(is.na(last_year) ~ as.integer(0), TRUE ~ last_year)) %>% 
-  # mutate(height = case_when(n == 1 ~ (bar_height/2) - bar_gap/2,
-  #                           TRUE ~ height),
-  #        y = case_when(n == 1 & n != last_year ~ y - bar_height/2 - bar_gap/2,
-  #                      TRUE ~ y))
-
 
 # fix years with multiple transitions -------------------------------------
 
@@ -168,107 +155,6 @@ shapes_multiple <-
 
 shapes_basic <- bind_rows(shapes_basic[-years_multiple, ], shapes_multiple)
 
-# years_multiple <- which(shapes_basic$n > 1)
-# 
-# shapes_multiple <- 
-#   shapes_basic[years_multiple, ] %>%
-#   filter(n > 1) %>% 
-#   mutate(height = height/(n+1) - bar_gap,
-#          y = y - seq(0, bar_height + bar_gap, length.out = 5)[1:4])
-# 
-# shapes_basic <- bind_rows(shapes_basic[-years_multiple, ], shapes_multiple)
-
-
-# -------------------------------------------------------------------------
-
-
-
-# shapes_basic <-
-#   tibble(year = 0:99,
-#          # x = {seq(from = margin_left,
-#          #         to = 1 - margin_right,
-#          #         by = (1  - margin_side)/n_long) + ((1 - margin_side)/n_long)*rect_margin} %>% .[1:100],
-#          x = rect_x,
-#          y = .9,
-#          # width = ((1 - margin_side)/n_long)*(1 - rect_margin),
-#          width = rect_width,
-#          height = bar_height,
-#          vjust = 1)
-
-# map data to rectangles ---------------------------------------------------
-
-# shapes_basic <-
-#   tibble(year = 0:99,
-#          # x = {seq(from = margin_left,
-#          #         to = 1 - margin_right,
-#          #         by = (1  - margin_side)/n_long) + ((1 - margin_side)/n_long)*rect_margin} %>% .[1:100],
-#          x = rect_x,
-#          y = .9,
-#          # width = ((1 - margin_side)/n_long)*(1 - rect_margin),
-#          width = rect_width,
-#          height = bar_height,
-#          vjust = 1) %>% 
-#   left_join(emps_toplot, by = "year") %>% 
-#   fill(name, .direction = "down") %>% 
-#   mutate(gp = map(name, make_gpar)) %>%
-#   mutate(transition = get_transition_year(name)) %>% 
-#   fill(transition, .direction = "up") %>% 
-#   mutate(last_year = c(transition[2:n()], transition[1]))
-#   # select(-name, -year)
-
-
-# add transition years ----------------------------------------------------
-
-# (more than one succession)
-# myears <- 
-#   shapes_basic %>% 
-#   count(year, sort = T) %>% 
-#   filter(n > 1) %>% 
-#   pull(year)
-# 
-# # half bar for start
-# shapes_transition <- 
-#   shapes_basic %>%
-#   filter(transition) %>%
-#   filter(!year %in% myears) %>% 
-#   mutate(y = y - 1/2*bar_height - bar_gap,
-#          height = 1/2*bar_height - bar_gap)
-# 
-# # half bar for end reign
-# shapes_last_year <- 
-#   shapes_basic %>% 
-#   filter(last_year) %>% 
-#   filter(!year %in% myears) %>%
-#   mutate(year = year + 1,
-#          height = 1/2*bar_height - bar_gap) %>% 
-#   select(-x) %>% 
-#   left_join(shapes_basic %>% select(year, x)) %>% 
-#   distinct(name, .keep_all = T)
-# 
-# # merge together
-# 
-# shapes_double <- 
-#   shapes_basic %>% 
-#   filter(!transition) %>% 
-#   rbind(shapes_transition) %>% 
-#   rbind(shapes_last_year) %>% 
-#   arrange(year) 
-# 
-# 
-# # more than 2 in a year ---------------------------------------------------
-# 
-# multi_year <- rbind(shapes_basic %>% filter(year %in% myears),
-#                     shapes_double %>% filter(year %in% myears))
-
-
-
-# remove extra columns ----------------------------------------------------
-
-# shapes_df <- 
-#   shapes_double %>% # prepare for plots
-#   select(-name, -year, -transition, -last_year, -month)
-  
-
 
 # remove extra columns ----------------------------------------------------
 
@@ -290,8 +176,6 @@ grid.newpage()
 # background grey rectangle
 grid.rect(gp = gpar(fill = bg_from_template, col = bg_from_template))
           
-# grid.rect(x = .1, y = .9, width = .1, height = .1, vjust = .5, hjust = .5)
-
 shapes_df %>% 
   pmap(grid.rect)
 dev.off()
