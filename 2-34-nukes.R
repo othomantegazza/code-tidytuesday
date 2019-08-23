347b8465a81460a7ac5beafd5624d916d8dd2da0
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-08-23 22:25:14 +0200

    draft text on path

--- 2-34-nukes.R
+++ 2-34-nukes.R
@@ -4,6 +4,7 @@ library(grid)
 library(tibbletime)
 library(maptools)
 library(sf)
+library(units)
 
 bg_col <- "#3752C3"
 fill_col <- "#5867A6"
@@ -93,7 +94,7 @@ nukes %>%
              fill = u)) +
   geom_bar()
 
-
+  
 
 # cumulative plot ---------------------------------------------------------
 
@@ -114,15 +115,76 @@ nukes2 <-
               stringsAsFactors = FALSE)) %>% 
   full_join(year_states) %>% 
   arrange(country, year) %>% 
-  fill(n_cumulative, .direction = "down")
+  fill(n_cumulative, .direction = "down") %>% 
+  # top 3 countries
+  filter(country %in% c( "USA", "USSR", "FRANCE"))
 
-nukes2 %>% 
+tst <-nukes2 %>% 
+  split(.$country) %>% 
+  # map(~loess(formula = n_cumulative ~ year, data = ., span = .2))
+  map(~smooth.spline(.$year, .$n_cumulative))
+
+predict(object = tst$USA, x = 1960, deriv = 1)
+
+
+# function to place text on path ------------------------------------------
+
+plot_ratio <- .03
+
+make_text_coords <- function(smooth_obj)
+  {
+  yrs <- nukes$year %>% unique()
+  n <- length(yrs)/2
+  start_at <- median(yrs)
+  
+  at_x <- rep(start_at, length.out = n)
+  
+  for(i in 2:n) {
+    at_x[i] <- at_x[i - 1] + 10 / predict(object = smooth_obj, x = at_x[i - 1], deriv = 1)$y
+  }
+  
+  tibble(year = at_x) %>% 
+    mutate(n_cumulative = predict(object = smooth_obj, x = year)$y,
+           d1 = predict(object = smooth_obj, x = year, deriv = 1)$y,
+           angle = d1*plot_ratio %>% 
+             atan() %>% as_units("radians") %>%
+             set_units("degrees"))
+  
+}
+
+text_data <- make_text_coords(tst$USA)
+
+# nukes3 <- 
+#   nukes2 %>% 
+#   group_by(country) %>% 
+#   mutate(smooth_n = loess(n_cumulative ~ year, span = .2)$fitted) %>% 
+#   mutate(dn = c(0, diff(smooth_n)/diff(year)))
+
+nukes3 <- 
+  tibble(year = nukes2 %>% filter(country == "USA") %>% pull(year)) %>% 
+  mutate(n_cumulative = predict(object = tst$USA, x = year)$y,
+         d1 = predict(object = tst$USA, x = year, deriv = 1)$y)
+
+nukes3 %>% 
   ggplot(aes(x = year,
-             y = n_cumulative,
-             colour = country)) +
-  geom_point() +
-  geom_smooth()
+             y = n_cumulative)) +
+             #colour = country)) +
+  geom_line() +
+  geom_line(aes(y = d1), colour = "red") +
+  geom_text(data = text_data,
+            aes(label = "A",
+                angle = angle),
+            vjust = 0,
+            hjust = 0) +
+  coord_fixed(ratio = plot_ratio)
+  # geom_point(shape = 2) 
+  # geom_point(aes(y = smooth_n)) +
+  # geom_point()
+  # geom_smooth()
 
+nukes3 %>% 
+filter(country == "FRANCE") %>% 
+  
 
 # many plots --------------------------------------------------------------
 
@@ -137,7 +199,7 @@ nukes2 %>%
 #     nukes %>%
 #     filter(month == month(my_in),
 #            year == year(my_in))
-#   
+#    
 #   print(d2)
 #   
 #   p <- p %>%
