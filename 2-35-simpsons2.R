05e5748a01452ac2f809cc2b04712e5342a36312
Otho Mantegazza <otho.mantegazza@gmail.com>
2019-08-28 18:46:16 +0200

    draft alternative version for week 35

---
+++ 2-35-simpsons2.R
@@ -0,0 +1,134 @@
+library(tidyverse)
+library(grid)
+library(showtext)
+
+
+font_add_google("Major Mono Display",
+                family = "titles")
+
+# get data ----------------------------------------------------------------
+
+data_url <- paste0('https://raw.githubusercontent.com/rfordatascience/tidytuesday/',
+                   'master/data/2019/2019-08-27/simpsons-guests.csv')
+
+data_path <- 'data/2-35-simpsons.Rdata'
+
+
+# Check if data have already been downloaded,
+# If not, read data from github and saves them locally
+if(!file.exists(data_path)) {
+  simpsons <- 
+    data_url %>% 
+    read_delim(data_url, delim = "|")#, quote = "")# , col_integer()))
+  
+  # download.file(data_url, csv_path)
+  
+  # simpsons <- read_delim(csv_path, delim = "|", quote = "")
+  
+  save(simpsons, file = data_path)
+} else {
+  load(data_path)
+}
+
+
+# tidy --------------------------------------------------------------------
+
+
+simpsons2 <- 
+  simpsons %>% 
+  mutate(season = as.numeric(season)) %>% 
+  separate(col = number, into = c("episode_count", "season_count"), sep = "–") %>% 
+  mutate(episode_count = as.numeric(episode_count)) %>% 
+  drop_na(season)
+
+
+
+# plot --------------------------------------------------------------------
+
+stars_in <- 
+  simpsons2 %>% 
+  group_by(guest_star) %>% 
+  count() %>% 
+  filter(n > 1) %>% 
+  pull(guest_star)
+
+violet <- "#6D1EC9"
+orange <- "#FF6C0D"
+blue <- "#263A89"
+
+colors <- rep(c(violet, orange, blue), length.out = max(simpsons2$season, na.rm = T))
+
+p <- 
+  simpsons2 %>% 
+  filter(guest_star %in% stars_in) %>%
+  ggplot(aes(x = guest_star %>% as_factor() %>% fct_rev() %>% as.numeric())) +
+  # lines for guest stars
+  geom_line(aes(y = episode_count,
+                group = guest_star),
+            size = .4,
+            alpha = .4,
+            colour = "grey30") +
+  # lines for episodes
+  geom_line(aes(y = episode_count,
+                group = episode_count,
+                colour = as.character(season)),
+            size = .2,
+            linetype = 1,
+            alpha = 1) +
+  # point each cross episode and guest star
+  geom_point(aes(y = episode_count,
+                 colour = as.character(season)),
+             size = .2,
+             # colour = "grey30",
+             alpha = .6) +
+  # guest star name
+  geom_text(data = . %>% group_by(guest_star) %>% filter(episode_count == min(episode_count)),
+            aes(y = episode_count,
+                label = guest_star),
+            size = 1.2,
+            hjust = 1.1,
+            vjust = .7,
+            angle = 45) +
+  # season mark
+  geom_segment(data = . %>% group_by(season) %>%
+              summarize(max_s = max(episode_count),
+                        min_s = min(episode_count)),
+              aes(x = 136,
+                  xend = 136,
+                  y = min_s,
+                  yend = max_s,
+                  colour = season %>% as.character()),
+              size = .6,
+              alpha = .7) +
+  # season label
+  geom_text(data = . %>% group_by(season) %>%
+                 summarize(y = mean(episode_count)),
+               aes(x = 137,
+                   label = paste("Season", season),
+                   y = y,
+                   colour = season %>% as.character()),
+               hjust = 0,
+               size = 2,
+               alpha = .7) +
+  scale_colour_manual(values = colors, guide = FALSE) +
+  theme_void()
+
+p
+
+svglite::svglite(file = "plots/2-35-simpsons3.svg",
+                 height = 20,
+                 width = 20)
+grid.newpage()
+p %>% print(vp = viewport(width = .6, height = .6, angle = 315))
+showtext_begin()
+str_wrap("The Simposons, by Matt Groening, have been running for more the 30 seasons
+         and had hundreds of guest stars, acting as themeselves or someone else.",
+         width = 15) %>% 
+  grid.text(hjust = 0, rot = 45,
+            x = .72, y = .72, 
+            gp = gpar(lineheight = 1,
+                      textsize = 2,
+                      fontfamily = "titles"))
+showtext_end()
+dev.off()
+ 
